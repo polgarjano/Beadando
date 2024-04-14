ca8326a7e26f037c96045d58853c5c1ec30cbb69
polgar <polgarjano@gmail.com>
2024-04-14 14:04:55 +0200

    validation and draw

---
+++ my_statistic.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+import json
+from jsonschema import validate, ValidationError
+import plotly.express as px
+
+
+
+schema = ""
+
+with open('./json/statisticInterface.json', 'r') as file:
+    lines = file.readlines()
+    schema ="".join(lines)
+    schema = json.loads(schema)
+
+print(schema)
+
+def is_valid(json_str):
+    try:
+        # Parse the JSON string
+        json_data = json.loads(json_str)
+        # Validate against the schema
+        validate(instance=json_data, schema=schema)
+        return True
+    except (ValidationError, json.JSONDecodeError):
+        return False
+
+
+def draw(json_string):
+     if not is_valid(json_string):
+         raise  ValidationError
+
+     json_data = json.loads(json_string)
+     df = pd.DataFrame(json_data['values'])
+     fig = px.line(df, x='x', y='y', color='classifier', markers=True,title=json_data['Title'])
+     fig.show()
+
+
+def readFromFile(path):
+    example = ""
+    with open(path, 'r') as file:
+        example = file.readlines()
+        example = " ".join(example)
+
+    return example
+
+
+if __name__ == '__main__':
+    print("hello")
+    draw(readFromFile('./json/example1.json'))
